31cc571f94de7d0944e309154b6ad808a0636e5d
Derek Finch <sagesolar@gmail.com>
2021-10-11 18:50:15 +0100

    Added sessions and users

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_190143) do
+ActiveRecord::Schema.define(version: 2021_10_11_174801) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2021_10_06_190143) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "is_admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
